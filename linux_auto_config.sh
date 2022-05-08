834931a424cb781b63cab5d7ccd3cbbadc0d9a42
richard <richardinruntime@gmail.com>
2022-05-08 16:10:24 +0800

    Add text-based browsers

--- linux_auto_config.sh
+++ linux_auto_config.sh
@@ -4,7 +4,7 @@
 sudo apt-get update && sudo apt-get dist-upgrade -y
 
 # install tools
-sudo apt install curl wget git zsh
+sudo apt install curl wget git zsh 
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/\
@@ -22,3 +22,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
 # https://www.youtube.com/watch?v=PZTLIVQxxEY
 # Above Youtube video provide a tutorial of how to configure the 
 # oh-my-zsh and powerlevel10k.
+
+# text-based web-browser, sometime it's fun to browse text website and 
+# documents and prevent the Javascript from  being executed by hacker.
+
+sudo apt install lynx elinks 

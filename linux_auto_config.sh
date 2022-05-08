440e2ee2c7c48a1f8612763ed1cec2d99c0d6092
richard <richardinruntime@gmail.com>
2022-05-08 16:00:30 +0800

    Add powerlevel10k fonts setting and oh-my-zsh setting

--- linux_auto_config.sh
+++ linux_auto_config.sh
@@ -18,4 +18,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git \
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
 	  $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-
+# zsh oh-my-zsh powerlevel10k make the terminal beautiful
+# https://www.youtube.com/watch?v=PZTLIVQxxEY
+# Above Youtube video provide a tutorial of how to configure the 
+# oh-my-zsh and powerlevel10k.
